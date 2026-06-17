5531a96ec51290f805e0a13d75b9caaf31a740bf
ramoniswack <ramontiwari086@gmail.com>
2026-06-17 10:31:45 +0000

    fix: set atomic=False on migration 0009 to prevent Postgres pending trigger events error

--- apps/tasks/migrations/0009_category_listing_kind.py
+++ apps/tasks/migrations/0009_category_listing_kind.py
@@ -38,6 +38,7 @@ def unseed_job_categories(apps, schema_editor):
 
 
 class Migration(migrations.Migration):
+    atomic = False
 
     dependencies = [
         ('tasks', '0008_task_cancellation_tracking'),
